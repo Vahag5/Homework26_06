102959ee1cfc075a507924876dce71213cfd8bd0
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-29 11:20:13 +0400

    Add files via upload

---
+++ 26_06_4.py
@@ -0,0 +1,14 @@
+counter = 0
+def increment_counter():
+    global counter
+    counter += 1
+    print(counter)
+
+
+increment_counter()     # 1
+increment_counter()     # 2
+increment_counter()     # 3
+increment_counter()     # 4
+increment_counter()     # 5
+increment_counter()     # 6
+increment_counter()     # 7
