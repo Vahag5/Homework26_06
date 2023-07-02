dd65af2a33ddbe9a66de778d6abed626330a53fb
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-07-02 13:31:34 +0400

    Update 26_06_5.py

--- 26_06_5.py
+++ 26_06_5.py
@@ -1,11 +1,10 @@
 def external_function(x):
     a = 10
     def inner_function():
-        return x * a
-    res = inner_function()
-    print(res)
-    return inner_function
-
+        result = x * a
+        return result
+    result = inner_function()
+    print(result)
 
 x = 5
 external_function(x)
